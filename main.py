93b334c25f7e01f6b25e21784deb62be11c541e2
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-04-19 09:23:23 -0500

    created and added two entries to database

--- main.py
+++ main.py
@@ -10,13 +10,43 @@ import requests
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://movies.db"
-Bootstrap5(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies.db"
 db.init_app(app)
+Bootstrap5(app)
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    
+    title = db.Column(db.String, unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String, nullable=False)
+    img_url = db.Column(db.String, nullable=False)
+
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+
+# second_movie = Movie(
+#     title="Avatar The Way of Water",
+#     year=2022,
+#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#     rating=7.3,
+#     ranking=9,
+#     review="I liked the water.",
+#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+# )
+
+# with app.app_context():
+#     db.session.add(second_movie)
+#     db.session.commit()
 
 
 @app.route("/")
